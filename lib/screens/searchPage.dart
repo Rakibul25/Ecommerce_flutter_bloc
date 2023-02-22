9887f471a8ceab9ef9882755346b00ca82caa60d
rakib25 <mislam171229@bscse.uiu.ac.bd>
2023-02-23 00:46:26 +0600

    scrollable search page on bloc state

--- lib/screens/searchPage.dart
+++ lib/screens/searchPage.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_flutter_bloc/logic/search/search_cubit/searchCubit.dar
 import 'package:ecommerce_flutter_bloc/logic/search/search_cubit/searchState.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
@@ -12,71 +13,84 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   String _searchQuery = '';
   List<String> _searchResults = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white70,
       body: SafeArea(
-        child: SizedBox(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: TextField(
-                  onChanged: (value) {
-                    _searchQuery = value;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    suffixIcon:  Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                onChanged: (value) {
+                  _searchQuery = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  suffixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
                     ),
-                    filled: true,
-                    fillColor: Colors.white,
                   ),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
               ),
-              BlocBuilder<searchCubit,searchState>(
-                builder: (context,state){
-                  if(state is ResultLoadingState){
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  if(state is ResultLoadedState){
-                    return Center(
-                      child: SingleChildScrollView(
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
+            ),
+            Expanded(
+              child: BlocBuilder<searchCubit, searchState>(builder: (context, state) {
+                if (state is ResultLoadingState) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (state is ResultLoadedState) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        GridView.builder(
+                          physics: NeverScrollableScrollPhysics(),
                           shrinkWrap: true,
                           itemCount: state.products!.data!.products!.results.length,
-                          itemBuilder: (context,index){
-                            return ListTile(
-                              title: Text(state.products!.data!.products!.results[index].productName.toString()),
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 300,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(state.products!.data!
+                                          .products!.results[index]!.image!
+                                          .toString()))),
                             );
                           },
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 10.0,
+                            crossAxisSpacing: 10.0,
+                            childAspectRatio: 1.0,
+                          ),
                         ),
-                      ),
-                    );
-                  }
-                  if(state is NoResultState){
-                    return Center(
-                      child: Text("error!"),
-                    );
-                  }
+                      ],
+                    ),
+                  );
+                }
+                if (state is NoResultState) {
                   return Center(
                     child: Text("error!"),
                   );
                 }
-              )
-            ],
-          ),
+                return Center(
+                  child: Text("error!"),
+                );
+              }),
+            )
+          ],
         ),
       ),
     );
